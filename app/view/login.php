4e737e1580edd2117bd1d2a1c1ef6229d31dc762
vyas senthil <vyas2senthil@gmail.com>
2012-08-17 10:45:31 +1200

    Adding the initial files to be commited

---
+++ app/view/login.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
+
+<html>
+    
+    
+    <body>
+        <form id="login">
+            
+            
+            
+            
+            
+            
+            
+        </form>
+      
+    </body>
+    
+    
+</html>
